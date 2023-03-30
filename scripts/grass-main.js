db7ee729db2b964099bcf1a67626c07dc3397bbb
Daniel Liang <44882266+dkhl65@users.noreply.github.com>
2023-03-30 13:54:52 -0400

    Gen 9 abilities

--- scripts/grass-main.js
+++ scripts/grass-main.js
@@ -1,70 +1,81 @@
 $(document).ready(() => {
-	// create the 50x50 plain dark background
-	for(let i = 0; i < 50; i++) {
-		for(let j = 0; j < 50; j++) {
-			if (i % 2 === 0 && j % 2 !== 0 || i % 2 !== 0 && j % 2 === 0) {
-				$("#editor-div").append('<img class="light grass-img" src="images/grass/light.png"></img>');
-			} else {
-				$("#editor-div").append('<img class="dark grass-img" src="images/grass/dark.png"></img>');
-			}
-		}
-		$("#editor-div").append("<br>");
-	}
-	
-	let grassDeployed = 0; // count how many grass tiles are used
-	
-	// deploy or reclaim individual grass tiles by clicking
-	$(".grass-img").click((ev) => {
-		const src = $(ev.target).prop("src");
-		if ((src.indexOf("dark.png") >= 0 || src.indexOf("light.png") >= 0) && grassDeployed < 50) {
-			$(ev.target).prop("src", "images/grass/grass.png");
-			grassDeployed++;
-		} else if(src.indexOf("grass.png") >= 0) {
-			if ($(ev.target).prop("class").indexOf("dark") >= 0) {
-				$(ev.target).prop("src", "images/grass/dark.png");
-			} else {
-				$(ev.target).prop("src", "images/grass/light.png");
-			}
-			grassDeployed--;
-		}
-		$("#remaining-grass-text").text(50 - grassDeployed);
-	});
-	
-	// prevent the middle mouse button from scrolling
-	$(".grass-img").on("mousedown", (ev) => {
-		if(ev.which === 2) {
-			return false;
-		}
-	});
-	
-	// hold middle mouse button and drag to deploy grass continuously
-	$(".grass-img").on("mousemove", (ev) => {
-		const src = $(ev.target).prop("src");
-		if(ev.which === 2 && (src.indexOf("dark.png") >= 0 || src.indexOf("light.png") >= 0) && grassDeployed < 50) {
-			$(ev.target).prop("src", "images/grass/grass.png");
-			grassDeployed++;
-		}
-		$("#remaining-grass-text").text(50 - grassDeployed);
-	});
-	
-	// press space bar to reclaim all grass tiles
-	$("body").keypress((ev) => {
-		if(ev.which === 32) {
-			$(".dark").prop("src", "images/grass/dark.png");
-			$(".light").prop("src", "images/grass/light.png");
-			grassDeployed = 0;
-			ev.preventDefault();
-		}
-		$("#remaining-grass-text").text(50 - grassDeployed);
-	});
-	
-	// show and hide help text
-	$("#help-button").click(() => {
-		if($("#help-div").is(":hidden")) {
-			$("#help-button").text("Hide Help");
-		} else {
-			$("#help-button").text("Show Help");
-		}
-		$("#help-div").toggle();
-	});
+  // create the 50x50 plain dark background
+  for (let i = 0; i < 50; i++) {
+    for (let j = 0; j < 50; j++) {
+      if ((i % 2 === 0 && j % 2 !== 0) || (i % 2 !== 0 && j % 2 === 0)) {
+        $("#editor-div").append(
+          '<img class="light grass-img" src="images/grass/light.png"></img>'
+        );
+      } else {
+        $("#editor-div").append(
+          '<img class="dark grass-img" src="images/grass/dark.png"></img>'
+        );
+      }
+    }
+    $("#editor-div").append("<br>");
+  }
+
+  let grassDeployed = 0; // count how many grass tiles are used
+
+  // deploy or reclaim individual grass tiles by clicking
+  $(".grass-img").click((ev) => {
+    const src = $(ev.target).prop("src");
+    if (
+      (src.indexOf("dark.png") >= 0 || src.indexOf("light.png") >= 0) &&
+      grassDeployed < 50
+    ) {
+      $(ev.target).prop("src", "images/grass/grass.png");
+      grassDeployed++;
+    } else if (src.indexOf("grass.png") >= 0) {
+      if ($(ev.target).prop("class").indexOf("dark") >= 0) {
+        $(ev.target).prop("src", "images/grass/dark.png");
+      } else {
+        $(ev.target).prop("src", "images/grass/light.png");
+      }
+      grassDeployed--;
+    }
+    $("#remaining-grass-text").text(50 - grassDeployed);
+  });
+
+  // prevent the middle mouse button from scrolling
+  $(".grass-img").on("mousedown", (ev) => {
+    if (ev.which === 2) {
+      return false;
+    }
+  });
+
+  // hold middle mouse button and drag to deploy grass continuously
+  $(".grass-img").on("mousemove", (ev) => {
+    const src = $(ev.target).prop("src");
+    if (
+      ev.which === 2 &&
+      (src.indexOf("dark.png") >= 0 || src.indexOf("light.png") >= 0) &&
+      grassDeployed < 50
+    ) {
+      $(ev.target).prop("src", "images/grass/grass.png");
+      grassDeployed++;
+    }
+    $("#remaining-grass-text").text(50 - grassDeployed);
+  });
+
+  // press space bar to reclaim all grass tiles
+  $("body").keypress((ev) => {
+    if (ev.which === 32) {
+      $(".dark").prop("src", "images/grass/dark.png");
+      $(".light").prop("src", "images/grass/light.png");
+      grassDeployed = 0;
+      ev.preventDefault();
+    }
+    $("#remaining-grass-text").text(50 - grassDeployed);
+  });
+
+  // show and hide help text
+  $("#help-button").click(() => {
+    if ($("#help-div").is(":hidden")) {
+      $("#help-button").text("Hide Help");
+    } else {
+      $("#help-button").text("Show Help");
+    }
+    $("#help-div").toggle();
+  });
 });
--- scripts/pokemon-header.js
+++ scripts/pokemon-header.js
@@ -3,93 +3,134 @@ const ND = 1; // normal damage
 const NV = 0.5; // not very effective
 const IM = 0; // immune
 const TYPES = [
-	"normal", "fire", "water", "grass", "electric", "ice", 
-	"fighting", "poison", "ground", "flying", "psychic", "bug", 
-	"rock", "ghost", "dragon", "dark", "steel", "fairy"
+  "normal",
+  "fire",
+  "water",
+  "grass",
+  "electric",
+  "ice",
+  "fighting",
+  "poison",
+  "ground",
+  "flying",
+  "psychic",
+  "bug",
+  "rock",
+  "ghost",
+  "dragon",
+  "dark",
+  "steel",
+  "fairy",
 ];
 const MATCHUPS = [
-//   NOR FIR WTR GRS ELC ICE FTN PSN GND FLY PSY BUG ROC GOS DRA DRK STL FAR
-	[ND, ND, ND, ND, ND, ND, ND, ND, ND, ND, ND, ND, NV, IM, ND, ND, NV, ND], // normal
-	[ND, NV, NV, SE, ND, SE, ND, ND, ND, ND, ND, SE, NV, ND, NV, ND, SE, ND], // fire
-	[ND, SE, NV, NV, ND, ND, ND, ND, SE, ND, ND, ND, SE, ND, NV, ND, ND, ND], // water
-	[ND, NV, SE, NV, ND, ND, ND, NV, SE, NV, ND, NV, SE, ND, NV, ND, NV, ND], // grass
-	[ND, ND, SE, NV, NV, ND, ND, ND, IM, SE, ND, ND, ND, ND, NV, ND, ND, ND], // electric
-	[ND, NV, NV, SE, ND, NV, ND, ND, SE, SE, ND, ND, ND, ND, SE, ND, NV, ND], // ice
-	[SE, ND, ND, ND, ND, SE, ND, NV, ND, NV, NV, NV, SE, IM, ND, SE, SE, NV], // fighting
-	[ND, ND, ND, SE, ND, ND, ND, NV, NV, ND, ND, ND, NV, NV, ND, ND, IM, SE], // poison
-	[ND, SE, ND, NV, SE, ND, ND, SE, ND, IM, ND, NV, SE, ND, ND, ND, SE, ND], // ground
-	[ND, ND, ND, SE, NV, ND, SE, ND, ND, ND, ND, SE, NV, ND, ND, ND, NV, ND], // flying
-	[ND, ND, ND, ND, ND, ND, SE, SE, ND, ND, NV, ND, ND, ND, ND, IM, NV, ND], // psychic
-	[ND, NV, ND, SE, ND, ND, NV, NV, ND, NV, SE, ND, ND, NV, ND, SE, NV, NV], // bug
-	[ND, SE, ND, ND, ND, SE, NV, ND, NV, SE, ND, SE, ND, ND, ND, ND, NV, ND], // rock
-	[IM, ND, ND, ND, ND, ND, ND, ND, ND, ND, SE, ND, ND, SE, ND, NV, ND, ND], // ghost
-	[ND, ND, ND, ND, ND, ND, ND, ND, ND, ND, ND, ND, ND, ND, SE, ND, NV, IM], // dragon
-	[ND, ND, ND, ND, ND, ND, NV, ND, ND, ND, SE, ND, ND, SE, ND, NV, ND, NV], // dark
-	[ND, NV, NV, ND, NV, SE, ND, ND, ND, ND, ND, ND, SE, ND, ND, ND, NV, SE], // steel
-	[ND, NV, ND, ND, ND, ND, SE, NV, ND, ND, ND, ND, ND, ND, SE, SE, NV, ND], // fairy
-	[ND, NV, SE, SE, ND, NV, ND, ND, SE, SE, ND, ND, ND, ND, SE, ND, NV, ND], // freeze-dry
-	[ND, SE, ND, NV, SE, ND, ND, SE, ND, ND, ND, NV, SE, ND, ND, ND, SE, ND] // thousand arrows
+  //   NOR FIR WTR GRS ELC ICE FTN PSN GND FLY PSY BUG ROC GOS DRA DRK STL FAR
+  [ND, ND, ND, ND, ND, ND, ND, ND, ND, ND, ND, ND, NV, IM, ND, ND, NV, ND], // normal
+  [ND, NV, NV, SE, ND, SE, ND, ND, ND, ND, ND, SE, NV, ND, NV, ND, SE, ND], // fire
+  [ND, SE, NV, NV, ND, ND, ND, ND, SE, ND, ND, ND, SE, ND, NV, ND, ND, ND], // water
+  [ND, NV, SE, NV, ND, ND, ND, NV, SE, NV, ND, NV, SE, ND, NV, ND, NV, ND], // grass
+  [ND, ND, SE, NV, NV, ND, ND, ND, IM, SE, ND, ND, ND, ND, NV, ND, ND, ND], // electric
+  [ND, NV, NV, SE, ND, NV, ND, ND, SE, SE, ND, ND, ND, ND, SE, ND, NV, ND], // ice
+  [SE, ND, ND, ND, ND, SE, ND, NV, ND, NV, NV, NV, SE, IM, ND, SE, SE, NV], // fighting
+  [ND, ND, ND, SE, ND, ND, ND, NV, NV, ND, ND, ND, NV, NV, ND, ND, IM, SE], // poison
+  [ND, SE, ND, NV, SE, ND, ND, SE, ND, IM, ND, NV, SE, ND, ND, ND, SE, ND], // ground
+  [ND, ND, ND, SE, NV, ND, SE, ND, ND, ND, ND, SE, NV, ND, ND, ND, NV, ND], // flying
+  [ND, ND, ND, ND, ND, ND, SE, SE, ND, ND, NV, ND, ND, ND, ND, IM, NV, ND], // psychic
+  [ND, NV, ND, SE, ND, ND, NV, NV, ND, NV, SE, ND, ND, NV, ND, SE, NV, NV], // bug
+  [ND, SE, ND, ND, ND, SE, NV, ND, NV, SE, ND, SE, ND, ND, ND, ND, NV, ND], // rock
+  [IM, ND, ND, ND, ND, ND, ND, ND, ND, ND, SE, ND, ND, SE, ND, NV, ND, ND], // ghost
+  [ND, ND, ND, ND, ND, ND, ND, ND, ND, ND, ND, ND, ND, ND, SE, ND, NV, IM], // dragon
+  [ND, ND, ND, ND, ND, ND, NV, ND, ND, ND, SE, ND, ND, SE, ND, NV, ND, NV], // dark
+  [ND, NV, NV, ND, NV, SE, ND, ND, ND, ND, ND, ND, SE, ND, ND, ND, NV, SE], // steel
+  [ND, NV, ND, ND, ND, ND, SE, NV, ND, ND, ND, ND, ND, ND, SE, SE, NV, ND], // fairy
+  [ND, NV, SE, SE, ND, NV, ND, ND, SE, SE, ND, ND, ND, ND, SE, ND, NV, ND], // freeze-dry
+  [ND, SE, ND, NV, SE, ND, ND, SE, ND, ND, ND, NV, SE, ND, ND, ND, SE, ND], // thousand arrows
 ];
-const ABILITIES = ["Lightning Rod", "Motor Drive", "Volt Absorb", "Storm Drain", "Water Absorb", "Dry Skin",
-"Fluffy", "Flash Fire", "Heatproof", "Water Bubble", "Thick Fat", "Levitate", "Sap Sipper", "Wonder Guard",
-"Scrappy", "Tinted Lens"];
+const ABILITIES = [
+  "Lightning Rod",
+  "Motor Drive",
+  "Volt Absorb",
+  "Storm Drain",
+  "Water Absorb",
+  "Dry Skin",
+  "Fluffy",
+  "Flash Fire",
+  "Well-Baked Body",
+  "Heatproof",
+  "Water Bubble",
+  "Thick Fat",
+  "Levitate",
+  "Earth Eater",
+  "Sap Sipper",
+  "Purifying Salt",
+  "Wonder Guard",
+  "Scrappy",
+  "Tinted Lens",
+];
 
 function getMatchup(attack, ability, type1, type2) {
-	let matchup = MATCHUPS[attack][type1];
-	if(!!type2) {
-		matchup *= MATCHUPS[attack][type2];
-	}
-	if(ability === "Tinted Lens" && matchup < 1) {
-		matchup *= 2;
-	} else if(ability === "Scrappy" && (type1 === TYPES.indexOf("ghost") || type2 ===  TYPES.indexOf("ghost")) && matchup === 0) {
-		matchup = 1;
-	}
-	return matchup;
+  let matchup = MATCHUPS[attack][type1];
+  if (!!type2) {
+    matchup *= MATCHUPS[attack][type2];
+  }
+  if (ability === "Tinted Lens" && matchup < 1) {
+    matchup *= 2;
+  } else if (
+    ability === "Scrappy" &&
+    (type1 === TYPES.indexOf("ghost") || type2 === TYPES.indexOf("ghost")) &&
+    matchup === 0
+  ) {
+    matchup = 1;
+  }
+  return matchup;
 }
 
 function getIneffectiveCoverage(attacks, abilities) {
-	if(attacks.length < 1) {
-		return [];
-	}
-	
-	let ineffective = [];
-	for(let type1 = 0; type1 < TYPES.length; type1++) {
-		//check effectiveness against a single type
-		let effective = false;
-		for(let i = 0; i < attacks.length; i++) {
-			if(getMatchup(attacks[i], abilities[i], type1) >= 1) {
-				effective = true;
-				break;
-			}
-		}
-		if(!effective && ineffective.indexOf(`${type1}`) < 0) {
-			for(let i = 0; i < ineffective.length; i++) {
-				if(ineffective[i].indexOf(`${type1}`) >= 0) {
-					ineffective.splice(i, 1);
-					i--;
-				}
-			}
-			ineffective.push(`${type1}`);
-			continue;
-		}
-		
-		// effectiveness for type combos
-		for(let type2 = 0; type2 < TYPES.length; type2++) {
-			effective = false;
-			if(type1 === type2 || ineffective.indexOf(`${type2}-${type1}`) > -1 || ineffective.indexOf(`${type2}`) > -1) {
-				continue;
-			}
-			for(let i = 0; i < attacks.length; i++) {
-				if(getMatchup(attacks[i], abilities[i], type1, type2) >= 1) {
-					effective = true;
-					break;
-				}
-			}
-			if(!effective) {
-				ineffective.push(`${type1}-${type2}`);
-			}
-		}
-	}
-	
-	return ineffective.slice();
+  if (attacks.length < 1) {
+    return [];
+  }
+
+  let ineffective = [];
+  for (let type1 = 0; type1 < TYPES.length; type1++) {
+    //check effectiveness against a single type
+    let effective = false;
+    for (let i = 0; i < attacks.length; i++) {
+      if (getMatchup(attacks[i], abilities[i], type1) >= 1) {
+        effective = true;
+        break;
+      }
+    }
+    if (!effective && ineffective.indexOf(`${type1}`) < 0) {
+      for (let i = 0; i < ineffective.length; i++) {
+        if (ineffective[i].indexOf(`${type1}`) >= 0) {
+          ineffective.splice(i, 1);
+          i--;
+        }
+      }
+      ineffective.push(`${type1}`);
+      continue;
+    }
+
+    // effectiveness for type combos
+    for (let type2 = 0; type2 < TYPES.length; type2++) {
+      effective = false;
+      if (
+        type1 === type2 ||
+        ineffective.indexOf(`${type2}-${type1}`) > -1 ||
+        ineffective.indexOf(`${type2}`) > -1
+      ) {
+        continue;
+      }
+      for (let i = 0; i < attacks.length; i++) {
+        if (getMatchup(attacks[i], abilities[i], type1, type2) >= 1) {
+          effective = true;
+          break;
+        }
+      }
+      if (!effective) {
+        ineffective.push(`${type1}-${type2}`);
+      }
+    }
+  }
+
+  return ineffective.slice();
 }
